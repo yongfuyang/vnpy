4c53094d292332abd132260af6bdd83b9c6944bd
yongfuyang <5309884@qq.com>
2017-06-22 07:51:16 +0800

    add yyf_turtle_tb

---
+++ examples/CtaBacktesting/runBacktesting_tt.py
@@ -0,0 +1,42 @@
+# encoding: UTF-8
+
+"""
+展示如何执行策略回测。
+"""
+
+from __future__ import division
+
+from vnpy.trader.app.ctaStrategy.ctaBacktesting import BacktestingEngine, OptimizationSetting, MINUTE_DB_NAME
+from vnpy.trader.app.ctaStrategy.strategy.strategy_yyf_turtle_tb import YYFTurtleStrategy
+
+if __name__ == '__main__':
+	
+
+	# 创建回测引擎
+	engine = BacktestingEngine()
+
+	# 设置引擎的回测模式为K线
+	engine.setBacktestingMode(engine.BAR_MODE)
+
+	# 设置回测用的数据起始日期
+	engine.setStartDate('20120101')
+	#engine.setEndDate('20120112')
+
+	# 设置产品相关参数
+	engine.setSlippage(0.2)     # 股指1跳
+	engine.setRate(1.0/10000)   # 万0.3
+	engine.setSize(300)         # 股指合约大小 
+	engine.setPriceTick(0.2)    # 股指最小价格变动
+
+	# 设置使用的历史数据库
+	engine.setDatabase(MINUTE_DB_NAME, 'IF0000')
+
+	# 在引擎中创建策略对象
+	d = {'atrLength': 11, "timeFrame":5}
+	engine.initStrategy(YYFTurtleStrategy, d)
+
+	# 开始跑回测
+	engine.runBacktesting()
+
+	# 显示回测结果
+	engine.showBacktestingResult()
---
+++ examples/CtaBacktesting/vnpyDataToTB.py
@@ -0,0 +1,24 @@
+#encoding: utf-8
+
+
+'''
+Form
+"Date","Time","Open","High","Low","Close","TotalVolume"
+2010-04-16,09:16:00,3450.0,3488.0,3450.0,3468.0,489
+'''
+
+'''
+To
+20100416,0.0916,3450.0,3488.0,3450.0,3468.0,489,30000
+'''
+import csv
+
+with open('IF0000_1min.csv') as file:
+	with open('IF0000_1min_tb_U.csv', 'w') as ofile:
+		writer = csv.writer(ofile)
+
+		file.readline()   #读掉第一行,下次再引用file的时候,将file的文件指针指向第二行开始的文件.
+		reader = csv.reader(file)
+		for Date , Time , Open , High , Low , Close , TotalVolume in reader:
+			writer.writerow([Date+" " + Time, Open , High , Low , Close , TotalVolume, TotalVolume])
+
