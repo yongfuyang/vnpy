e4cf79277dc31dbe8c108c6595c972a9d4f393a3
yongfuyang <5309884@qq.com>
2017-06-22 21:27:12 +0800

    modify turtle

--- examples/CtaBacktesting/runBacktesting_tt.py
+++ examples/CtaBacktesting/runBacktesting_tt.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 
 	# 设置回测用的数据起始日期
 	engine.setStartDate('20120101')
-	#engine.setEndDate('20120112')
+	engine.setEndDate('20120201')
 
 	# 设置产品相关参数
 	engine.setSlippage(0.2)     # 股指1跳
